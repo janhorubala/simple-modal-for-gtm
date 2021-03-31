c41e9c3b4727bb30d742365f06593d47d021868e
JakubK <jakubkuniszewski1@gmail.com>
2021-03-31 16:30:02 +0200

    Make script work on a set of modals

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const modal = document.getElementsByClassName('modal')[0];
-modal.style.display = 'none';
-
-const popup = modal.getElementsByClassName('modal__popup')[0];
-popup.addEventListener('click', e => {
-    e.stopPropagation();
-})
-
-modal.addEventListener('click', e => {
+const modals = Array.from(document.querySelectorAll('.modal'));
+modals.forEach(modal => {
     modal.style.display = 'none';
-})
+    const popup = modal.querySelector('.modal__popup');
 
-const close = modal.getElementsByClassName('modal__close')[0];
-close.addEventListener('click', e => {
-    modal.style.display = 'none';
-})
+    popup.addEventListener('click', e => {
+        e.stopPropagation();
+    })
+
+    modal.addEventListener('click', e => {
+        modal.style.display = 'none';
+    })
+    const close = modal.querySelector('.modal__close');
+    close.addEventListener('click', e => {
+        modal.style.display = 'none';
+    })
+});
 
 const showModal = document.getElementById('showModal');
 showModal.addEventListener('click', e => {
-    modal.style.display = 'flex';
+    modals[0].style.display = 'flex';
 });
